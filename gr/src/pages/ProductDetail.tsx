eddb5d3dc5f0f3b3af7426972e3493f7a2f2f8b7
LeeSuBin-khu <subini1707@khu.ac.kr>
2022-09-14 23:28:57 +0900

    리뷰 & 통계 기능 최종 완료

--- gr/src/pages/ProductDetail.tsx
+++ gr/src/pages/ProductDetail.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/layout/Header";
 import "../assets/css/review.css";
 import "../assets/css/statistics.css";
 import ProductView from "../components/product/ProductView";
+import Review from "../components/review/Review";
+import StatisticsMain from "../components/statistics/Main";
 
 const Detail: React.FC = () => {
+  const [reviewUpdate, setReviewUpdate] = useState<number>(0);
   return (
     <div className="product-detail-container">
       <ProductView />
+      <StatisticsMain reviewUpdate={reviewUpdate} />
+      <Review setReviewUpdate={setReviewUpdate} />
     </div>
   );
 };
