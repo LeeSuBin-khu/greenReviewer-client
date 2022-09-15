5a8e89189d1cf61fe4ad3ae85f406e6581ed8f31
Sunny-jinn <rlawlsdn316@gmail.com>
2022-09-15 14:52:26 +0900

    [fix] 최종 수정

--- gr/src/components/product/ProductList.tsx
+++ gr/src/components/product/ProductList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import ProductCard from "./ProductCard";
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { IProductState, productActions, ProductState } from "../../store";
 
@@ -22,10 +22,10 @@ const ProductList: React.FC = () => {
             size: 10,
           },
         })
-        .then((res) => {
+        .then((res: AxiosResponse) => {
           dispatch(productActions.setProductList(res.data));
         })
-        .catch((err) => console.log(err));
+        .catch((err: AxiosError) => console.log(err));
     };
     getProductList();
   }, []);
--- gr/src/components/product/ProductView.tsx
+++ gr/src/components/product/ProductView.tsx
@@ -19,11 +19,14 @@ const ProductView = (): JSX.Element => {
   const params = useParams();
   const reviewRef = useRef<HTMLDivElement>(null);
   const [productDetail, setProductDetail] = useState<IProductDetail>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const getProductDetail = async () => {
       await axios.get(`/product/detail/${params.id}`).then((res) => {
         setProductDetail(res.data);
+        console.log("hi");
+        setLoading(true);
       });
     };
     getProductDetail();
@@ -35,50 +38,56 @@ const ProductView = (): JSX.Element => {
 
   return (
     <div>
-      <div className="product-detail-card">
-        <div className="product-detail-thumb">
-          <img src={productDetail?.pic_url} alt="Loading..." />
-        </div>
-        <div>
-          <div className="product-detail-name">
-            <p>{productDetail?.name}</p>
+      {!loading ? (
+        <p>잠시만 기다려주세요...</p>
+      ) : (
+        <>
+          <div className="product-detail-card">
+            <div className="product-detail-thumb">
+              <img src={productDetail?.pic_url} alt="Loading..." />
+            </div>
+            <div>
+              <div className="product-detail-name">
+                <p>{productDetail?.name}</p>
+              </div>
+              <div className="product-detail-vendor">
+                <p>{productDetail?.vendor}</p>
+              </div>
+              <div className="product-detail-review">
+                <p>리뷰 수: {productDetail?.reviewer}</p>
+              </div>
+              <div className="product-detail-delivery">
+                <p>배송비: {productDetail?.deliveryFee.toLocaleString()}원</p>
+              </div>
+              <div className="product-detail-price">
+                <p>{productDetail?.price.toLocaleString()}원</p>
+              </div>
+              <div className="product-detail-btn">
+                <a
+                  href={
+                    productDetail?.originalURL ? productDetail?.originalURL : ""
+                  }
+                  target="_blank"
+                >
+                  <button className="product-detail-buybtn">구매하기</button>
+                </a>
+                <button
+                  className="product-detail-reviewbtn"
+                  onClick={reviewBtnClick}
+                >
+                  리뷰확인
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="product-detail-vendor">
-            <p>{productDetail?.vendor}</p>
+          <div className="text-center">
+            {productDetail?.detailpicUrl.map((list) => (
+              <img src={list} alt="Loading..." />
+            ))}
           </div>
-          <div className="product-detail-review">
-            <p>리뷰 수: {productDetail?.reviewer}</p>
-          </div>
-          <div className="product-detail-delivery">
-            <p>배송비: {productDetail?.deliveryFee.toLocaleString()}원</p>
-          </div>
-          <div className="product-detail-price">
-            <p>{productDetail?.price.toLocaleString()}원</p>
-          </div>
-          <div className="product-detail-btn">
-            <a
-              href={
-                productDetail?.originalURL ? productDetail?.originalURL : ""
-              }
-              target="_blank"
-            >
-              <button className="product-detail-buybtn">구매하기</button>
-            </a>
-            <button
-              className="product-detail-reviewbtn"
-              onClick={reviewBtnClick}
-            >
-              리뷰확인
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="text-center">
-        {productDetail?.detailpicUrl.map((list) => (
-          <img src={list} alt="Loading..." />
-        ))}
-      </div>
-      <div ref={reviewRef}></div>
+          <div ref={reviewRef}></div>
+        </>
+      )}
     </div>
   );
 };
