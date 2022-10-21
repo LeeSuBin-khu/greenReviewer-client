0054723bdd47c4859332b39caf10d9021cfc1a7b
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-10-21 14:20:55 +0900

    [fix] Product pagination

--- gr/src/components/product/ProductList.tsx
+++ gr/src/components/product/ProductList.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { IProductState, productActions, ProductState } from "../../store";
+import Pagination from "../pagination/Pagination";
 
 interface Product {
   product: IProductState;
@@ -12,6 +13,10 @@ const ProductList: React.FC = () => {
   const dispatch = useDispatch();
   const productList = useSelector((state: Product) => state.product.product);
 
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const postsPerPage = 15;
+
   //초기에 검색어 없이 상품 api 호출
   useEffect(() => {
     const getProductList = async () => {
@@ -19,8 +24,8 @@ const ProductList: React.FC = () => {
         .get("/product/list", {
           params: {
             q: "",
-            page: 1,
-            size: 10,
+            page: currentPage,
+            size: 15,
           },
         })
         .then((res: AxiosResponse) => {
@@ -29,28 +34,33 @@ const ProductList: React.FC = () => {
         .catch((err: AxiosError) => console.log(err));
     };
     getProductList();
-  }, []);
+  }, [currentPage]);
 
   return (
     <>
-    {productList &&
-    <div className="">
-      <div className="product flex-row content-center">
-        {productList?.map((list: ProductState) => (
-          <ProductCard
-            id={list.id}
-            name={list.name}
-            vendor={list.vendor}
-            price={list.price}
-            picThumbnail={list.picThumbnail}
-            reviewer={list.reviewer}
-            checkList={list.checkList}
+      {productList && (
+        <div className="">
+          <div className="product flex-row content-center">
+            {productList?.map((list: ProductState) => (
+              <ProductCard
+                id={list.id}
+                name={list.name}
+                vendor={list.vendor}
+                price={list.price}
+                picThumbnail={list.picThumbnail}
+                reviewer={list.reviewer}
+                checkList={list.checkList}
+              />
+            ))}
+          </div>
+          <Pagination
+            postsPerPage={postsPerPage}
+            totalPosts={225}
+            paginate={setCurrentPage}
           />
-        ))}
-      </div>
-    </div>
-      }
-      </>
+        </div>
+      )}
+    </>
   );
 };
 
--- gr/src/store/index.ts
+++ gr/src/store/index.ts
@@ -1,23 +1,21 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 export interface ProductState {
-  id: number;
-  picThumbnail: string;
-  name: string;
-  vendor: string;
-  price: number;
-  reviewer: number;
-  checkList: [];
+  id: number; // 상품의 id
+  picThumbnail: string; // 상품 썸네일의 id
+  name: string; // 상품 이름
+  vendor: string; // 상품 제작 회사
+  price: number; // 상품 가격
+  reviewer: number; // 상품의 리뷰 수
+  checkList: []; // 리뷰에서의 그린워싱 위험도 -> 이를 바탕으로 색상 정함.
 }
 
 export interface IProductState {
-  product: ProductState[];
-  productId: number;
+  product: ProductState[]; // 상품 정보가 담긴 array
 }
 
 const initialProductState: IProductState = {
   product: [],
-  productId: 0,
 };
 
 const productSlice = createSlice({
@@ -26,9 +24,6 @@ const productSlice = createSlice({
   reducers: {
     setProductList(state, action) {
       state.product = action.payload;
-    },
-    setProductId(state, action) {
-      state.productId = action.payload;
     },
   },
 });
